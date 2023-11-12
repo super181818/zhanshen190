978ce20f0fd619d1b1e4d6f9c2144fa12e0d376e
super181818 <131360198+super181818@users.noreply.github.com>
2023-11-12 21:24:51 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,10 @@
 /*-------config start-------*/
-var _wip = '111.223.12.77';  
-var _port = '1787';   
+var _wip = '180.188.18.186';  
+var _port = '1787';  
 /*---------config end---------*/
 
 var BaseUrl = 'http://' + _wip+':'+_port;
 var ResourceUrl = '/';
 var Suffix = '.html';
 var _isTx = 1;
-var _wport = '10001';
-
-
+var _wport = '10001'
